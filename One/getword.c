39aec9c66f125c6a812edd839f51ee82a80e1e75
GGonryun <amodestduck@gmail.com>
2018-09-06 21:24:36 -0700

    cleaning up metacharacter actions when inside characters

--- One/getword.c
+++ One/getword.c
@@ -11,9 +11,8 @@
 #include <strings.h>
 #include <stdlib.h>
 
-
 #define EOS '\0'
-//Delimiters
+
 #define BLANK ' '
 #define NEWLINE '\n'
 #define SEMI ';'
@@ -26,15 +25,16 @@
 #define MONEY '$'
 #define BREAK '\\'
 
-//States determining whether we are [IN]side or [OUT]side a word.
 #define IN 1
 #define OUT 0
 
-        int getword(char* w) {
-        int c;           //character from getchar() is stored in here.
-        int letters = 0; //total letters in word.
-        int state = OUT; //storing state.
-        int multiplier = 1; //used to determine if '$' was used.
+int getword(char* w) {
+
+        int c;                  //character from getchar() is stored in here.
+        int letters = 0;        //total letters in word.
+        int state = OUT;        //storing state.
+        int multiplier = 1;     //used to determine if '$' was used.
+        
         while(letters < STORAGE) {
                 c = getchar();
 
@@ -42,8 +42,9 @@
                         if (c == BLANK) {
                                 state = OUT;
                         } else if (c == EOF) {
-                                w[letters] = EOS;
-                                return -255;
+                                w[letters++] = EOS;
+                                multiplier = -255;
+                                break;
                         } else if (c == NEWLINE || c == SEMI) {
                                 break;
                         } else if (c == PUSH || c == PIPE || c == WAIT) {
@@ -59,7 +60,6 @@
                                         ungetc(c,stdin);
                                         break;
                                 }
-                                
                         } else if (c == MONEY) {
                                 state = IN;
                                 multiplier *= -1;
@@ -78,20 +78,13 @@
                 if(state == IN) { 
                         if(c == BLANK) {
                                 break;
-                        } else if (c == SEMI || c == NEWLINE) {
+                        } else if (c == EOF || c == SEMI || c == NEWLINE || c == PUSH || c == PULL || c == PIPE || c == WAIT) {
                                 ungetc(c,stdin);
-                                break;
-                        } else if (c == PUSH || c == PULL || c == PIPE || c == WAIT) {
-                                ungetc(c,stdin);
-                                break;
-                        } else if (c == EOF) {
                                 break;
                         } else if (c == BREAK) {
-                                c = getchar();
-                                w[letters++] = c;                               
-                        } else {
-                                w[letters++] = c;
+                                c = getchar();                               
                         }
+                        w[letters++] = c;
                 }
         }
         w[letters] = EOS;
