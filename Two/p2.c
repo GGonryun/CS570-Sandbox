c6fd572c016842333cc1d94789efd162931ff7ea
GGonryun <amodestduck@gmail.com>
2018-09-17 15:38:15 -0700

    cleaned up forking & child processes

--- Two/p2.c
+++ Two/p2.c
@@ -6,13 +6,20 @@ void empty_array(char * a[]);
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "getword.h"
 
 #define MAX 10
 #define LENGTH 255
+#define TRUE 1
+#define FALSE 0
 
 char *command[MAX]; //to be executed.
-int terminate = 0;
+/* global flags */
+int f_wait;
+
+
+
 int main() 
 {
         (void) build_array(command);
@@ -23,13 +30,36 @@ int main()
 
 int shell_ui() {
         int l;
+        int kidpid;
+
         for(;;) {
-                prompt(); l = parse();
+                /* reset global flags */
+                f_wait = FALSE;
+                kidpid = 0;
+               
                 /* preparing user input for execution */
-                if(l <= 0)      
+                prompt(); l = parse();
+                if(0 >= l)      
                         continue;
-                if(*command[0] == EOF)
+                if(EOF == *command[0])
                         break;
+                
+                /* executing command */        
+                if(-1 == (kidpid = (int)fork())) {      //fork process.
+                        exit(1);                        //cannot fork.
+                } else if ( 0 == kidpid) {              //in child. 
+                        (void) sleep(2);
+                        exit(0);
+                } else {                                //in parent.
+                        printf("parent is locked out.\n");
+                        for(;f_wait>0;) {                       //waiting for child process.
+                                pid_t pid;
+                                pid = wait(NULL);
+                                if(pid == kidpid)
+                                        break;
+                        }
+                        printf("parent is ready now. \n");
+                }
         }
 }
 
@@ -38,10 +68,17 @@ int parse()
         int i;
         int l;
         for(i = 0; l = getword(command[i]); i++) {
-                if(l==0) 
-                        break;                 // reached the end of command.
-                if(l==-255)
+                
+                /* terminating command */
+                if(0 == l) 
+                        break;                
+                else if(-255 == l) {
                         *command[i] = EOF;
+                        break;
+                }
+                /* activating global flags for execution */
+                if(0 == strcmp(command[i],"&")) 
+                        f_wait++;
         }
         return i;
 }
