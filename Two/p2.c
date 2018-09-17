a24bfc605c750d8c5ccbe9a080466ded3217202f
GGonryun <amodestduck@gmail.com>
2018-09-17 09:34:24 -0700

    clean build

--- Two/p2.c
+++ Two/p2.c
@@ -1,19 +1,29 @@
 int parse();
+void prompt();
+void build_array(char * a[]);
+void empty_array(char * a[]);
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "getword.h"
 
 #define MAX 10
 #define LENGTH 255
 
-char line[MAX][LENGTH];
+char *line[MAX];
 
 int main() 
 {
-        int i, j;
-        for(i = 0, j = parse(); i < j ; i++)
-                printf("%s\n", line[i]);       
+        (void) build_array(line);
+        for(;;) {
+                prompt();
+                int i, j;
+                for(i = 0, j = parse(); i < j ; i++)
+                        printf("%s ", line[i]);
+                printf("\n");       
+        }
+        (void) empty_array(line);
 }
 
 
@@ -27,3 +37,21 @@ int parse()
         }
         return i;
 }
+
+
+void prompt() {
+        printf(":cs570 :");
+}
+
+void build_array(char *a[]) {
+        int i;
+        for(i = 0; i < MAX; i++)
+                a[i] = (char *) malloc (LENGTH*sizeof(char));
+}
+
+void empty_array(char * a[]) {
+        int i;
+        for(i = 0; i < MAX; i++)
+                free(a[i]);
+}
+
